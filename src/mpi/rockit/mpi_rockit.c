c777b345a9bacf26e383e9b89cb80d4e55c43f18
lijun <lijun@oray.com>
2026-04-20 16:20:53 +0800

    refactor: rename stub functions to rockit in mpi_rockit.c

--- src/mpi/rockit/mpi_rockit.c
+++ src/mpi/rockit/mpi_rockit.c
@@ -8,7 +8,7 @@
  *
  * @author: LiJun
  * @date: 2026-03-20
- * @fileName: mpi_stub.c
+ * @fileName: mpi_rockit.c
  * @description: mpi 模拟
  */
 
@@ -19,54 +19,54 @@
 
 #include "log/log_tag.h"
 
-static int stub_sys_init(void)
+static int rockit_sys_init(void)
 {
     WriteLog(LOG_INFO, "sys_init");
     return 0;
 }
 
-static int stub_sys_deinit(void)
+static int rockit_sys_deinit(void)
 {
     WriteLog(LOG_INFO, "sys_deinit");
     return 0;
 }
 
-static int stub_vi_init(void *ctx)
+static int rockit_vi_init(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "vi_init");
     return 0;
 }
 
-static int stub_vi_deinit(void *ctx)
+static int rockit_vi_deinit(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "vi_deinit");
     return 0;
 }
 
-static void stub_vi_release_data(void *ctx, void *data, int len)
+static void rockit_vi_release_data(void *ctx, void *data, int len)
 {
     (void)ctx;
     (void)data;
     (void)len;
 }
 
-static int stub_vpss_init(void *ctx)
+static int rockit_vpss_init(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "vpss_init");
     return 0;
 }
 
-static int stub_vpss_deinit(void *ctx)
+static int rockit_vpss_deinit(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "vpss_deinit");
     return 0;
 }
 
-static int stub_vpss_get_data(void *ctx, void *data, int *len)
+static int rockit_vpss_get_data(void *ctx, void *data, int *len)
 {
     (void)ctx;
     (void)data;
@@ -74,28 +74,28 @@ static int stub_vpss_get_data(void *ctx, void *data, int *len)
     return -1;
 }
 
-static void stub_vpss_release_data(void *ctx, void *data, int len)
+static void rockit_vpss_release_data(void *ctx, void *data, int len)
 {
     (void)ctx;
     (void)data;
     (void)len;
 }
 
-static int stub_venc_init(void *ctx)
+static int rockit_venc_init(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "venc_init");
     return 0;
 }
 
-static int stub_venc_deinit(void *ctx)
+static int rockit_venc_deinit(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "venc_deinit");
     return 0;
 }
 
-static int stub_venc_get_data(void *ctx, void *data, int *len)
+static int rockit_venc_get_data(void *ctx, void *data, int *len)
 {
     (void)ctx;
     /* 桩：模拟无数据，返回 -1 让 stream_venc 线程 sleep；实际平台从编码器取数 */
@@ -106,7 +106,7 @@ static int stub_venc_get_data(void *ctx, void *data, int *len)
     return 0;
 }
 
-static int stub_venc_release_data(void *ctx, void *data, int len)
+static int rockit_venc_release_data(void *ctx, void *data, int len)
 {
     (void)ctx;
     (void)data;
@@ -114,35 +114,35 @@ static int stub_venc_release_data(void *ctx, void *data, int len)
     return 0;
 }
 
-static int stub_ai_init(void *ctx)
+static int rockit_ai_init(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "ai_init");
     return 0;
 }
 
-static int stub_ai_deinit(void *ctx)
+static int rockit_ai_deinit(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "ai_deinit");
     return 0;
 }
 
-static int stub_aenc_init(void *ctx)
+static int rockit_aenc_init(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "aenc_init");
     return 0;
 }
 
-static int stub_aenc_deinit(void *ctx)
+static int rockit_aenc_deinit(void *ctx)
 {
     (void)ctx;
     WriteLog(LOG_INFO, "aenc_deinit");
     return 0;
 }
 
-static int stub_aenc_get_data(void *ctx, void *data, int *len)
+static int rockit_aenc_get_data(void *ctx, void *data, int *len)
 {
     (void)ctx;
     (void)data;
@@ -150,14 +150,14 @@ static int stub_aenc_get_data(void *ctx, void *data, int *len)
     return -1;
 }
 
-static void stub_ai_release_data(void *ctx, void *data, int len)
+static void rockit_ai_release_data(void *ctx, void *data, int len)
 {
     (void)ctx;
     (void)data;
     (void)len;
 }
 
-static int stub_aenc_release_data(void *ctx, void *data, int len)
+static int rockit_aenc_release_data(void *ctx, void *data, int len)
 {
     (void)ctx;
     (void)data;
@@ -166,42 +166,42 @@ static int stub_aenc_release_data(void *ctx, void *data, int len)
 }
 
 
-static struct mpi_sys_opt stub_sys_opt = {
-    .sys_init = stub_sys_init,
-    .sys_deinit = stub_sys_deinit,
+static struct mpi_sys_opt rockit_sys_opt = {
+    .sys_init = rockit_sys_init,
+    .sys_deinit = rockit_sys_deinit,
 };
 
-static struct mpi_video_opt stub_video_opt = {
-    .vi_init = stub_vi_init,
-    .vi_deinit = stub_vi_deinit,
-    .vi_release_data = stub_vi_release_data,
-    .vpss_init = stub_vpss_init,
-    .vpss_deinit = stub_vpss_deinit,
-    .vpss_get_data = stub_vpss_get_data,
-    .vpss_release_data = stub_vpss_release_data,
-    .venc_init = stub_venc_init,
-    .venc_deinit = stub_venc_deinit,
-    .venc_get_data = stub_venc_get_data,
-    .venc_release_data = stub_venc_release_data,
+static struct mpi_video_opt rockit_video_opt = {
+    .vi_init = rockit_vi_init,
+    .vi_deinit = rockit_vi_deinit,
+    .vi_release_data = rockit_vi_release_data,
+    .vpss_init = rockit_vpss_init,
+    .vpss_deinit = rockit_vpss_deinit,
+    .vpss_get_data = rockit_vpss_get_data,
+    .vpss_release_data = rockit_vpss_release_data,
+    .venc_init = rockit_venc_init,
+    .venc_deinit = rockit_venc_deinit,
+    .venc_get_data = rockit_venc_get_data,
+    .venc_release_data = rockit_venc_release_data,
 };
 
-static struct mpi_audio_opt stub_audio_opt = {
-    .ai_init = stub_ai_init,
-    .ai_deinit = stub_ai_deinit,
-    .ai_release_data = stub_ai_release_data,
-    .aenc_init = stub_aenc_init,
-    .aenc_deinit = stub_aenc_deinit,
-    .aenc_get_data = stub_aenc_get_data,
-    .aenc_release_data = stub_aenc_release_data,
+static struct mpi_audio_opt rockit_audio_opt = {
+    .ai_init = rockit_ai_init,
+    .ai_deinit = rockit_ai_deinit,
+    .ai_release_data = rockit_ai_release_data,
+    .aenc_init = rockit_aenc_init,
+    .aenc_deinit = rockit_aenc_deinit,
+    .aenc_get_data = rockit_aenc_get_data,
+    .aenc_release_data = rockit_aenc_release_data,
 };
 
-static struct mpi_intf stub_mpi = {
-    .sys = &stub_sys_opt,
-    .video = &stub_video_opt,
-    .audio = &stub_audio_opt,
+static struct mpi_intf rockit_mpi = {
+    .sys = &rockit_sys_opt,
+    .video = &rockit_video_opt,
+    .audio = &rockit_audio_opt,
 };
 
 struct mpi_intf *mpi_intf_create(void)
 {
-    return &stub_mpi;
+    return &rockit_mpi;
 }
